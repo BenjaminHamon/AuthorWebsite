e6bd2f8bfb627168d220a5e518947b53b4cb36f8
Benjamin Hamon <development@benjaminhamon.com>
2026-08-03 15:16:47 +0200

    Group metrics by endpoint instead of raw path

--- Website/benjaminhamon_author_website/application_factory.py
+++ Website/benjaminhamon_author_website/application_factory.py
@@ -33,9 +33,9 @@ def create_application(metrics_token: str, server: Optional[str] = None) -> Appl
 
 def create_metrics(server: Optional[str] = None) -> PrometheusMetrics:
     if server is None:
-        return PrometheusMetrics(None, metrics_decorator = metrics_authorization)
+        return PrometheusMetrics(None, group_by = "endpoint", metrics_decorator = metrics_authorization)
     if server == "gunicorn":
-        return GunicornInternalPrometheusMetrics(None, metrics_decorator = metrics_authorization)
+        return GunicornInternalPrometheusMetrics(None, group_by = "endpoint", metrics_decorator = metrics_authorization)
     raise ValueError("Unsupported server: '%s'" % server)
 
 
